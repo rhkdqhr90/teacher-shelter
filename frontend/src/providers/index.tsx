3448e796a87254644390edc98c2f7499e2e56fe5
Gwangkyo <rhkdqhr08@gmail.com>
2026-02-22 19:40:27 +0900

    fix: Remove empty lines between imports

--- frontend/src/providers/index.tsx
+++ frontend/src/providers/index.tsx
@@ -1,11 +1,9 @@
 'use client';
 
 import { type ReactNode, useEffect } from 'react';
-
 import { Toaster } from '@/components/ui/toaster';
 import { setupGlobalErrorHandlers } from '@/lib/logger';
 import { initWebVitals } from '@/lib/web-vitals';
-
 import { AuthProvider } from './auth-provider';
 import { QueryProvider } from './query-provider';
 import { ThemeProvider } from './theme-provider';
